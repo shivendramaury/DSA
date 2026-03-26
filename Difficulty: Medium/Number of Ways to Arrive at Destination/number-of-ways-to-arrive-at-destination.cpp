1972294af11653490fa90e3de4deffedf3cad478
Shivendra <shivendramaury@gmail.com>
2026-03-26 10:20:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Ways to Arrive at Destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        
+        vector<pair<int, int>> adj[n];
+        for(auto it : roads){
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>,
+        greater<pair<int, int>>> pq;
+        
+        vector<int> dist(n, 1e9), ways(n,0);
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0,0});
+        
+        int mod = (int)(1e9+7);
+        
+        while(!pq.empty()){
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            
+            for(auto it : adj[node]){
+                int adjNode = it.first;
+                int edW = it.second;
+                
+                if(dis + edW < dist[adjNode]){
+                    dist[adjNode] = dis + edW;
+                    pq.push({dis + edW, adjNode});
+                    ways[adjNode] = ways[node];
+                }
+                else if(dis + edW == dist[adjNode]){
+                    ways[adjNode] = ways[adjNode] + ways[node];
+                }
+            }
+        }
+        return ways[n-1] % mod;
+    }
+};
