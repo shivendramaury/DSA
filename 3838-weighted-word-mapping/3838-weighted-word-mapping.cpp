d35e57b5f77e0c278adf81941303fdf2b38c694d
Shivendra <shivendramaury@gmail.com>
2026-06-13 06:29:28 +0530

    Time: 0 ms (100%), Space: 43.6 MB (51.01%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for(string word : words){
+            int sum=0;
+
+            for(char ch: word) {
+                sum += weights[ch - 'a'];
+            }
+
+            int res = sum % 26;
+
+            ans.push_back('z' - res);
+        }
+        return ans;
+
+    }
+};
