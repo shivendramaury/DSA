dbc3926805f14a1802a186732c427be9181d8977
Shivendra <shivendramaury@gmail.com>
2026-06-25 10:51:04 +0530

    Time: 1429 ms (20.29%), Space: 37.3 MB (79.48%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        // int n=nums.size();
+        // int count=0;
+
+        // for(int i=0;i<n;i++){
+        //     int sum=0;
+        //     for(int j=i;j<n;j++){
+        //         sum+=nums[j];
+        //         if(sum == k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+
         int n=nums.size();
         int count=0;
-
+        int sum=0;
+        vector<int> preSum;
         for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum == k){
+            sum+=nums[i];
+            preSum.push_back(sum);
+        }
+
+        for(int j=0;j<n;j++){
+            if(preSum[j]  == k){
+                count++;
+            }
+            for(int i=0;i<j;i++){
+                if(preSum[j] - preSum[i] == k){
                     count++;
                 }
             }
+
         }
+
+
         return count;
     }
 };
