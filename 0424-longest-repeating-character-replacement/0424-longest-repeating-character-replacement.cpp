bb3f5fa61fbf3f6ea42c1968b0e470d692d5d0f4
Shivendra <shivendramaury@gmail.com>
2026-06-27 10:26:30 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.96%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.length();
+        int maxFreq = 0;
+        int left = 0;
+        int maxLength=0;
+
+        vector<int> freq(26,0);
+
+        for(int right = 0 ; right<n; right++){
+            freq[s[right]-'A']++;
+
+            maxFreq = max(maxFreq, freq[s[right]-'A']);
+
+            while((right-left+1)-maxFreq>k){
+                freq[s[left]-'A']--;
+                left++;
+            }
+            maxLength = max(maxLength, right-left+1);
+        }
+        return maxLength;
+    }
+};
