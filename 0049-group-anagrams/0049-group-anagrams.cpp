71c10af8637946a7f2de5e99640c12c8a63bf30e
Shivendra <shivendramaury@gmail.com>
2026-07-01 10:26:17 +0530

    Time: 9 ms (94.78%), Space: 24.9 MB (79.84%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> mpp;
+
+        for(auto s: strs){
+            string key = s;
+            sort(key.begin(), key.end());
+            mpp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+
+        for(auto &it: mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
