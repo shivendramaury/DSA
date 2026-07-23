f25c3cebde50bee7ad796deee1e3d06bad56c83d
Shivendra <shivendramaury@gmail.com>
2026-07-23 15:54:21 +0530

    Time: 0 ms (100%), Space: 198.8 MB (60.63%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n<=2) return n;
+
+        int near = 1;
+
+        while(near <= n){
+            near <<=1;
+        }
+
+        return near;
+    }
+};
