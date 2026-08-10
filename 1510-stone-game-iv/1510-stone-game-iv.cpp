457c3aa60d0abc461d15088003887216be463011
Shivendra <shivendramaury@gmail.com>
2026-08-10 20:14:35 +0530

    Time: 173 ms (17.6%), Space: 16.7 MB (45.73%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> t;
+    bool solve(int n){
+
+        if(n == 0){
+            return false;
+        }
+
+
+        if(t[n] != -1){
+            return t[n] == 1 ? true : false;
+        }
+        
+        for(int k=1; k*k<=n; k++){
+            if(solve(n-(k*k)) == false){
+                return t[n] = true;
+            }
+        }
+        return false;
+    }
+    bool winnerSquareGame(int n) {
+        t.assign(n+1, -1);
+        return solve(n);
+    }
+};
