53b4cd9ce81645763dbb9fadafc5a04295f07e9b
Shivendra <shivendramaury@gmail.com>
2026-08-17 00:16:27 +0530

    Time: 4 ms (62.44%), Space: 131.3 MB (33.5%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        
+        int n = stones.size();
+        int c0=0, c1=0, c2=0;
+
+        for(int i=0; i<n; i++){
+            if(stones[i] % 3 == 0){
+                c0++;
+            }
+            else if(stones[i] % 3 == 1){
+                c1++;
+            }
+            else{
+                c2++;
+            }
+        }
+
+        if(c1 == 0 && c2 == 0){
+            return false;
+        }
+
+        if(c0 % 2 == 0){
+            return c1>0 && c2>0;
+        }
+
+        return abs(c1-c2)>2;
+    }
+};
